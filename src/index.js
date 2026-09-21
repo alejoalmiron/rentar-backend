36aec384dbe86f45a1dcaf96955a14941198113b
ttomi03 <titopalma2024@gmail.com>
2026-09-21 00:41:09 -0300

    feat: implementado GraphQL para consulta de disponibilidad (Integrante 4)

--- src/index.js
+++ src/index.js
@@ -5,25 +5,43 @@ import swaggerUi from 'swagger-ui-express';
 import { PrismaClient } from '@prisma/client';
 import { swaggerSpec } from './config/swagger.js';
 
+// GraphQL
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs } from './graphql/schema.js';
+import { resolvers } from './graphql/resolvers.js';
+
 dotenv.config();
 
 const app = express();
 export const prisma = new PrismaClient();
 
-// Middlewares
-app.use(cors());
-app.use(express.json());
 
-// Documentación Swagger
+app.use(cors({ origin: '*' }));
+
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Ruta base de prueba
+
 app.get('/', (req, res) => {
   res.json({ mensaje: 'API Rentar funcionando correctamente' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-  console.log(`📄 Documentación Swagger lista en http://localhost:${PORT}/api-docs`);
-});
+async function startServer() {
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers,
+    introspection: true
+  });
+  
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`📄 Documentación Swagger lista en http://localhost:${PORT}/api-docs`);
+    console.log(`🎯 GraphQL Sandbox listo en http://localhost:${PORT}/graphql`);
+  });
+}
+
+startServer();
